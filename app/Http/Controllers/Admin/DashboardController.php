3ea908f6ec5ca181dd8750f27ef3e26959facf63
ratebBarakat <ratebbarakat2021@gmail.com>
2023-03-26 11:55:20 +0300

    add some charts

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -26,6 +26,16 @@ class DashboardController extends Controller
                 ->whereBetween('date',[Carbon::now()->subMonth(),Carbon::now()])
                 ->groupBy('blood_type')
                 ->get();
+
+            $expireBlood = Donation::select('blood_type', DB::raw('SUM(quantity) AS total_expire'))
+                ->where('expire_at', '<', Carbon::now()->format('y-m-d'))
+                ->groupBy('blood_type')
+                ->get();
+    
+            $nonExpireBlood = Donation::select('blood_type', DB::raw('SUM(quantity) AS total_non_expire'))
+                ->where('expire_at', '>=', Carbon::now()->format('y-m-d'))
+                ->groupBy('blood_type')
+                ->get();
         } else {
 
         
@@ -54,9 +64,29 @@ class DashboardController extends Controller
                 ->whereBetween('date',[Carbon::now()->subMonth(),Carbon::now()])
                 ->groupBy('blood_type')
                 ->get();
+
+            $expireBlood = Donation::select('blood_type', DB::raw('SUM(quantity) AS total_expire'))
+                ->where('expire_at', '<', Carbon::now()->format('y-m-d'))
+                ->whereIn('center_id', function($query) use ($user,$centerId) {
+                    $query->select('id')
+                        ->from('donation_centers')
+                        ->where('id', $centerId);
+                })
+                ->groupBy('blood_type')
+                ->get();
+    
+            $nonExpireBlood = Donation::select('blood_type', DB::raw('SUM(quantity) AS total_non_expire'))
+                ->where('expire_at', '>=', Carbon::now()->format('y-m-d'))
+                ->whereIn('center_id', function($query) use ($user,$centerId) {
+                    $query->select('id')
+                        ->from('donation_centers')
+                        ->where('id', $centerId);
+                })
+                ->groupBy('blood_type')
+                ->get();
         }
         $query = DB::getQueryLog();
-        return view('admin.dashboard',compact('donationsWeek','donationsMounth','query'));
+        return view('admin.dashboard',compact('donationsWeek','donationsMounth','expireBlood', 'nonExpireBlood'));
     }
     
 }
