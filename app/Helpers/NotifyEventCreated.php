c18ac27e2dfc7a9894b14740c4c21ad5c98cba2f
ratebBarakat <ratebbarakat2021@gmail.com>
2023-05-16 11:24:48 +0300

    send notitfication when event created to only availalbe users where last appointment is 2 moutns ago

--- app/Helpers/NotifyEventCreated.php
+++ app/Helpers/NotifyEventCreated.php
@@ -6,13 +6,19 @@ use App\Models\Appointment;
 use App\Models\Event;
 use App\Models\User;
 use App\Notifications\notifyEventCreated as NotificationsNotifyEventCreated;
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Support\Facades\Notification;
 
 class NotifyEventCreated 
 {
     public static function notifyUsers(Event $event)
     {
-        $users = User::get();
+        $users = User::whereDoesntHave('appointments')
+                ->orWhereHas('appointments', function (Builder $query) {
+                    $query->latest('id')->limit(1)->where('date', '<', Carbon::now()->subMonths(2));
+                })
+                ->get();
         Notification::send($users, (new NotificationsNotifyEventCreated($event)));
     }
 }
