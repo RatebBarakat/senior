c80b86ffe18329b6fa90004dd56a631c5cc89f23
RatebBarakat <ratebbarakat2021@gmail.com>
2023-07-09 13:53:48 +0300

    update routes for changes

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,6 @@
 use App\Http\Controllers\Api\Admin\UserController;
 use App\Http\Controllers\Api\AppointmentController;
 use App\Http\Controllers\Api\ProfileController;
-use App\Http\Controllers\Api\SocialLoginController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\Admin\LoginController;
@@ -23,54 +22,58 @@ use App\Models\DonationCenter;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-Route::get('/centers',function () {
-    if(cache()->has('centers')){
-        return CenterResource::collection(cache()->get('centers'));
-    }
+use Illuminate\Support\Facades\URL;
+use Carbon\Carbon;
+
+Route::get('/centers', function () {
+    // if(cache()->has('centers')){
+    // return CenterResource::collection(cache()->get('centers'));
+    // }
     $centers = DonationCenter::get();
-    cache()->set('centers',$centers);
+    // cache()->set('centers',$centers);
     return CenterResource::collection($centers);
 });
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::prefix('admin')->group(function (){
-    Route::post('/login',[LoginController::class,'Adminlogin']);
+Route::prefix('admin')->group(function () {
+    Route::post('/login', [LoginController::class, 'Adminlogin']);
 });
-Route::prefix('user')->group(function (){
-    Route::post('/login',[LoginController::class,'Userlogin']);
+Route::prefix('user')->group(function () {
+    Route::post('/login', [LoginController::class, 'Userlogin']);
 });
 
-Route::get('/dash',function (){
+Route::get('/dash', function () {
     return response()->json('im authorize');
 })->middleware(['auth:sanctum']);
 
-Route::middleware(['auth:sanctum','api.admin'])->group(function (){
-    Route::apiResource('/users',UserController::class);
+Route::middleware(['auth:sanctum', 'api.admin'])->group(function () {
+    Route::apiResource('/users', UserController::class);
 });
 
-Route::middleware(['auth:sanctum','verified'])->prefix('user')->name('user.')->group(function (){
+Route::middleware(['auth:sanctum', 'verified'])->prefix('user')->name('user.')->group(function () {
 
     Route::prefix('profile')->name('profile.')->group(function () {
-        Route::post('/',[ProfileController::class,'update'])->name('update');
-        Route::apiResource('',ProfileController::class)->only('index');
+        Route::post('/', [ProfileController::class, 'update'])->name('update');
+        Route::apiResource('', ProfileController::class)->only('index');
     });
 
-    Route::get('/notification',[NotificationController::class,'index']);
+    Route::get('/notification', [NotificationController::class, 'index']);
 
     Route::name('appointment.')->group(function () {
-        Route::post('/appointment/download/{id}',[AppointmentController::class,'downloadPdf'])->name('download');
-        Route::apiResource('/appointment',AppointmentController::class);
+        Route::post('/appointment/availalbe-time', [AppointmentController::class, 'getAvailableTimes'])
+            ->name('available');
+        Route::post('/appointment/download/{id}', [AppointmentController::class, 'downloadPdf'])
+            ->name('download');
+        Route::apiResource('/appointment', AppointmentController::class);
     });
 
-    Route::name('request.')->group(function ()
-    {
-        Route::apiResource('/request',BloodRequestController::class);
+    Route::name('request.')->group(function () {
+        Route::apiResource('/request', BloodRequestController::class);
     });
 });
 
-Route::middleware('guest:sanctum')->group(function ()
-{
-    Route::post('/user/register',[RegisterController::class,'register']);
+Route::middleware('guest:sanctum')->group(function () {
+    Route::post('/user/register', [RegisterController::class, 'register']);
 });
--- routes/web.php
+++ routes/web.php
@@ -2,25 +2,19 @@
 
 use App\Http\Controllers\Admin\AdminPasswordController;
 use App\Http\Controllers\Admin\BloodRequestController as AdminBloodRequestController;
-use App\Http\Controllers\Admin\LocationController;
-use App\Http\Controllers\Admin\ProfileController;
 use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\ExcelController;
+use App\Http\Controllers\Admin\LocationController;
 use App\Http\Controllers\Admin\NotificationController;
-use App\Http\Controllers\Api\BloodRequestController;
+use App\Http\Controllers\Admin\ProfileController;
 use App\Http\Controllers\Api\SocialLoginController;
 use App\Http\Controllers\Center\ReportController;
-use App\Mail\SendPdfEmail as MailSendPdfEmail;
-use App\Models\Admin;
 use App\Models\Permission;
 use App\Models\Role;
 use App\Models\User;
-use App\Notifications\SendPdfEmail;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Storage;
-use Laravel\Socialite\Facades\Socialite;
-use Illuminate\Foundation\Auth\EmailVerificationRequest;
 
 /*
 |--------------------------------------------------------------------------
@@ -33,29 +27,56 @@ use Illuminate\Foundation\Auth\EmailVerificationRequest;
 |
 */
 
+Route::get('/email/verify/{id}/{expiry}/{token}', function ($id, $expiry, $token) {
+
+    if (!request()->hasValidSignature()) {
+        abort(404);
+    }
+
+    $user = User::find($id);
+
+    if (!$user || !hash_hmac('sha256', $user->getEmailForVerification() . $expiry, env('APP_KEY')) === $token) {
+        return 'Invalid verification link.';
+    }
+
+    if ($user->email_verified_at) {
+        return 'Your email has already been verified.';
+    }
+
+    $user->email_verified_at = now();
+    $user->save();
+
+    return redirect()->to('http://localhost:8080/verify-email');
+})->name('verification.verify')->middleware(['signed']);
+
+Route::post('/email/verification-notification', function (Request $request) {
+    $request->user()->sendEmailVerificationNotification();
+
+    return back()->with('message', 'Verification link sent!');
+})->middleware(['auth', 'throttle:6,1'])->name('verification.send');
 
 Route::middleware('guest:admin')->get('/', function () {
     return view('admin.login');
 })->name('home');
 
-Route::post('/admin/login',[\App\Http\Controllers\Controller::class,'login'])
+Route::post('/admin/login', [\App\Http\Controllers\Controller::class, 'login'])
     ->name('admin.login');
 
 
-Route::get('/success',function (){
-   return view('success');
+Route::get('/success', function () {
+    return view('success');
 })->name('success');
 
-Route::get('/seed',function (){
-    $Permission = Permission::create(['name' => 'request-event','slug' => 'request-event']);
-    $allowsRoles = array('hospital','center-admin');
+Route::get('/seed', function () {
+    $Permission = Permission::create(['name' => 'request-event', 'slug' => 'request-event']);
+    $allowsRoles = array('hospital', 'center-admin');
 
     foreach ($allowsRoles as $role) {
-        Role::where('name',$role)->first()->permissions()->attach($Permission);
+        Role::where('name', $role)->first()->permissions()->attach($Permission);
     }
 });
 
-Route::post('/admin/login',[\App\Http\Controllers\Controller::class,'login'])
+Route::post('/admin/login', [\App\Http\Controllers\Controller::class, 'login'])
     ->name('admin.login');
 
 Route::get('auth/{provider}', [SocialLoginController::class, 'redirectToProvider'])
@@ -63,28 +84,25 @@ Route::get('auth/{provider}', [SocialLoginController::class, 'redirectToProvider
 Route::get('auth/{provider}/callback', [SocialLoginController::class, 'handleCallback'])
     ->name('google.callback');
 
-Route::get('/login',function (){
-   \Illuminate\Support\Facades\Auth::guard('admin')->login(\App\Models\Admin::first());
+Route::get('/login', function () {
+    \Illuminate\Support\Facades\Auth::guard('admin')->login(\App\Models\Admin::first());
 });
 
-Route::get('/admin/set-password/{id}/{token}',[AdminPasswordController::class,'show'])->name('admin.setPassword');
-Route::post('/admin/set-password/{id}/{token}',[AdminPasswordController::class,'setPassword'])->name('admin.storePassword');
+Route::get('/admin/set-password/{id}/{token}', [AdminPasswordController::class, 'show'])->name('admin.setPassword');
+Route::post('/admin/set-password/{id}/{token}', [AdminPasswordController::class, 'setPassword'])->name('admin.storePassword');
 
 
-Route::middleware('auth:admin')->prefix('admin')->name('admin.')->group(function (){
+Route::middleware('auth:admin')->prefix('admin')->name('admin.')->group(function () {
 
-    Route::middleware('can:request-event')->name('event.')->prefix('event')->group(function ()
-    {
+    Route::middleware('can:request-event')->name('event.')->prefix('event')->group(function () {
         Route::view('/create', 'admin.event.create-event')->name('create');
     });
 
-    Route::prefix('notification')->name('notification.')->group(function ()
-    {
-        Route::put('/{id}/markAsRead',[NotificationController::class,'markAsRead'])->name('markRead');
+    Route::prefix('notification')->name('notification.')->group(function () {
+        Route::put('/{id}/markAsRead', [NotificationController::class, 'markAsRead'])->name('markRead');
     });
 
-    Route::prefix('messages')->name('messages.')->group(function ()
-    {
+    Route::prefix('messages')->name('messages.')->group(function () {
         Route::view('/', 'admin.messages')->name('index');
     });
 
@@ -96,94 +114,66 @@ Route::middleware('auth:admin')->prefix('admin')->name('admin.')->group(function
         return redirect('/');
     })->name('logout');
 
-   Route::get('/',[DashboardController::class,'index'])->name('index');
+    Route::get('/', [DashboardController::class, 'index'])->name('index');
 
-//    Route::prefix('blood')->name('request.')->group(function ()
-//    {
-//         Route::get('/show/{id}',[BloodRequestController::class,'show'])->name('show');
-//    });
+    //    Route::prefix('blood')->name('request.')->group(function ()
+    //    {
+    //         Route::get('/show/{id}',[BloodRequestController::class,'show'])->name('show');
+    //    });
 
-    Route::get('/profile/{id}',[ProfileController::class,'viewProfile'])->name('profile.show');
-    Route::get('/profile/',[ProfileController::class,'index'])
+    Route::get('/profile/{id}', [ProfileController::class, 'viewProfile'])->name('profile.show');
+    Route::get('/profile/', [ProfileController::class, 'index'])
         ->name('profile');
 
 
-   Route::middleware('can:super-admin')->group(function (){
+    Route::middleware('can:super-admin')->group(function () {
 
 
-        Route::prefix('events')->name('events.')->group(function ()
-        {
-            Route::view('/','admin.super.events')->name('index');            
+        Route::prefix('events')->name('events.')->group(function () {
+            Route::view('/', 'admin.super.events')->name('index');
         });
 
-        Route::prefix('location')->name('location.')->group(function (){
-            Route::get('/',[LocationController::class,'index'])->name('index');
-            Route::view('/create','admin.location.create-location')->name('create');
-            Route::post('/create',[LocationController::class,'store'])->name('store');
-            Route::get('/edit/{id}',[LocationController::class,'edit'])->name('edit');
-            Route::post('/update/{id}',[LocationController::class,'update'])->name('update');
-            Route::post('/delete/{id}',[LocationController::class,'delete'])->name('delete');
+        Route::prefix('location')->name('location.')->group(function () {
+            Route::get('/', [LocationController::class, 'index'])->name('index');
+            Route::view('/create', 'admin.location.create-location')->name('create');
+            Route::post('/create', [LocationController::class, 'store'])->name('store');
+            Route::get('/edit/{id}', [LocationController::class, 'edit'])->name('edit');
+            Route::post('/update/{id}', [LocationController::class, 'update'])->name('update');
+            Route::post('/delete/{id}', [LocationController::class, 'delete'])->name('delete');
         });
-       Route::view('/roles','admin.super.roles')->name('roles');
+        Route::view('/roles', 'admin.super.roles')->name('roles');
 
-       Route::prefix('centers')->name('centers.')->group(function (){
-           Route::view('/','admin.super.centers')->name('index');
-           Route::view('/create','admin.super.create-center')->name('create');
-       });
+        Route::prefix('centers')->name('centers.')->group(function () {
+            Route::view('/', 'admin.super.centers')->name('index');
+            Route::view('/create', 'admin.super.create-center')->name('create');
+        });
 
-       Route::view('/admins','admin.super.admins')->name('admins');
-   });
+        Route::view('/admins', 'admin.super.admins')->name('admins');
+    });
 
-   Route::middleware(['can:center-admin','centerAdmin'])->name('admincenter.')
-       ->prefix('center')->group(function (){
-      Route::view('/','admin.center.index')->name('index');
-      Route::view('/report','admin.center.reports')->name('reports');
-      Route::post('/report/create',[ReportController::class,'generateReport'])->name('reports.store');
-   });
+    Route::middleware(['can:center-admin', 'centerAdmin'])->name('admincenter.')
+        ->prefix('center')->group(function () {
+            Route::view('/', 'admin.center.index')->name('index');
+            Route::view('/report', 'admin.center.reports')->name('reports');
+            Route::post('/report/create', [ReportController::class, 'generateReport'])
+            ->name('reports.store');
+            Route::name('excel.')->group(function () {
+                Route::get('/excel', [ExcelController::class, 'index'])->name('index');
+                Route::get('/excel/create', [ExcelController::class, 'create'])->name('create');
+            });
+        });
 
-    Route::middleware(['can:center-employee','employee'])->group(function () {
-        Route::prefix('blood-request')->name('blood-request.')->group(function ()
-        {
+    Route::middleware(['can:center-employee', 'employee'])->group(function () {
+        Route::prefix('blood-request')->name('blood-request.')->group(function () {
             Route::view('/', 'admin.blood-request.index')->name('index');
-            Route::get('/{id}',[AdminBloodRequestController::class,'show'])->name('show');
-            Route::post('/unlock',[AdminBloodRequestController::class,'unLock'])->name('unLock');
+            Route::get('/{id}', [AdminBloodRequestController::class, 'show'])->name('show');
+            Route::post('/unlock', [AdminBloodRequestController::class, 'unLock'])->name('unLock');
         });
 
-        Route::name('appointments.')->group(function ()
-        {
-            Route::view('/appointments','admin.appointment.index')->name('index');            
+        Route::name('appointments.')->group(function () {
+            Route::view('/appointments', 'admin.appointment.index')->name('index');
         });
-   });
+    });
 });
-
-Route::get('/email/verify/{id}/{expiry}/{token}', function ($id, $expiry, $token) {
-    $user = User::find($id);
-
-    if (!$user || !hash_hmac('sha256', $user->getEmailForVerification().$expiry, env('APP_KEY')) === $token) {
-        // if the user is not found or the email hash doesn't match, return an error message
-        return 'Invalid verification link.';
-    }
-
-    if ($user->email_verified_at) {
-        // if the user's email is already verified, return a message saying so
-        return 'Your email has already been verified.';
-    }
-
-    $user->email_verified_at = now();
-    $user->save();
-
-    return redirect()->to('http://localhost:8080/verify-email');
-    
-})->name('verification.verify')->middleware(['signed']);
-
-
-
-
- 
-Route::post('/email/verification-notification', function (Request $request) {
-    $request->user()->sendEmailVerificationNotification();
- 
-    return back()->with('message', 'Verification link sent!');
-})->middleware(['auth', 'throttle:6,1'])->name('verification.send');
 
 include __DIR__ . "/user.php";
