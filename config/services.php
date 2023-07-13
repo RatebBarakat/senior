0261f3165963d82d0806ad7a908e027a4f826b91
RatebBarakat <ratebbarakat2021@gmail.com>
2023-07-13 16:09:21 +0300

    configure social credentials for api login

--- config/services.php
+++ config/services.php
@@ -34,11 +34,11 @@ return [
     'google' => [
         'client_id' => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-        'redirect' => 'http://127.0.0.1:8000/auth/google/callback',
+        'redirect' => 'http://localhost:8080/google/callback',
     ],
     'facebook' => [
         'client_id' => env('FACEBOOK_CLIENT_ID'),
         'client_secret' => env('FACEBOOK_CLIENT_SECRET'),
-        'redirect' => 'http://localhost:8000/auth/facebook/callback',
+        'redirect' => 'http://localhost:8080/google/callback',
     ],
 ];
